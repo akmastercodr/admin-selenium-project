2b2fdec3fe4a2d83310c2b5abf5d4c226afac504
ayush kumar <ayushkumarak0607@gmail.com>
2026-03-19 17:38:26 +0530

    pip

--- src/main/java/com/dams/base/BaseTest.java
+++ src/main/java/com/dams/base/BaseTest.java
@@ -5,7 +5,9 @@ import com.dams.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(com.dams.report.ExtentListener.class)
 public class BaseTest {
     protected WebDriver driver;
     protected LoginPage loginPage;
